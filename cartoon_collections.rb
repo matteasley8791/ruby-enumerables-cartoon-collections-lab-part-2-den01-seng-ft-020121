92b89ac281486e415168129ee3991b9c5389ac16
Matthew Easley <matteasley8791@gmail.com>
2021-01-23 18:32:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
- planeteer_calls.any? { |call| 
+ planeteer_calls.any? { |call| cal.lenghjt > 4 } 
 end
 
 def find_valid_calls(planeteer_calls)
