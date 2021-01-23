423371440e6f12f3c3bb20056b54b8f914827746
Matthew Easley <matteasley8791@gmail.com>
2021-01-23 18:34:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find { |call| calls.include
+  planeteer_calls.find { |call| calls.include?(call) }
 end
