27fb2c6ede2e48e23c5b85a06927c7970eabe982
Matthew Easley <matteasley8791@gmail.com>
2021-01-23 18:38:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| call.capatalize + '!' }
+  planeteer_calls.map { |call| call.capatalize + "!" }
 end
 
 def long_planeteer_calls(planeteer_calls)
